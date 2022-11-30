81ce20c2825ed8db8a100eec3a03ccbc31a3627d
parthkl021 <92939004+parthkl021@users.noreply.github.com>
2022-11-30 18:17:37 +0530

    Add files via upload

---
+++ ass3_q1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <sched.h>
+#include <time.h>
+#include <stdlib.h>
+int random ;
+int main(){
+
+}
+
+
+
+
+
+
+
+
+// bas thread use karke system call karni hai
+
+// program1(){
+//     char arr[50][6] = {"quiet","wheat","orbit","tasty","chase","cross","siege","flush","merit","vague","slime","thigh","aloof","front","clean","begin","rifle","troop","storm","chest","clash","outer","coach","bacon","braid","light","still","ridge","leash","bowel","fibre","smart","march","worth","enter","party","Koran","shake","cause","shaft","medal","trace","novel","bring","peace","feign","stain","large","fault","craft"};
+//     for (int i = 0; i < 50; i++)
+//     {
+//         printf("%s\n",arr[i]);
+//     }
+//     random = rand() % 45;
+//     char *str = arr[random];
+//     char *str1 = arr[random + 1];
+//     char *str2 = arr[random + 2];
+//     char *str3 = arr[random + 3];
+//     char *str4 = arr[random + 4];
+
+//     program2(str,str1,str2,str3,str4,random);
+// }
---
+++ prog2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <sched.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(){ 
+
+}
+// program2(char one,char two, char three, char four, char five, int index){
+    
+//     printf("The string is: %s, The indx is: %d\n",one,index);
+//     printf("The string is: %s, The indx is: %d\n",two,index + 1);
+//     printf("The string is: %s, The indx is: %d\n",three,index + 2);
+//     printf("The string is: %s, The indx is: %d\n",four,index + 3);
+//     printf("The string is: %s, The indx is: %d\n",five,index + 4);
+// }
